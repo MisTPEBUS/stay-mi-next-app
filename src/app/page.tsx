450807e5b6336028556b6238fd9639d78f3880c3
ttppoo121440 <ttppoo121440@gmail.com>
2025-04-15 23:58:54 +1000

    test:重整專案

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">首頁</h1>
     </div>
   );
-}
+};
+
+export default Home;
