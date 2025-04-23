acbf545c9a1e5735626d2631f8a2d46d478ff85f
chindesu0207 <147689292+chindesu0207@users.noreply.github.com>
2025-04-23 12:20:33 +0800

    refactor: 💡 調整名稱 home 改為 (home)，FlowSection 改為 SetpSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import FaqSection from "./home/sections/FaqSection";
-import FlowSection from "./home/sections/FlowSection";
-import Hero from "./home/sections/Hero";
+import FaqSection from "./(home)/sections/FaqSection";
+import Hero from "./(home)/sections/Hero";
+import StepSection from "./(home)/sections/StepSection";
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <Hero />
-      <FlowSection />
+      <StepSection />
       <FaqSection />
     </div>
   );
