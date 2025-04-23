ade44a30c38c1e8e353653efa7eef261d6ddbced
Lobinda <lobinda123@gmail.com>
2025-04-23 14:23:59 +0800

    style: 💄 修改登入表單留白地方

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./_components/LoginForm/page";
 
 const Login = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#F5F5F5] px-4">
+    <div className="flex justify-center bg-[#F5F5F5] px-4 py-4">
       <LoginForm />
     </div>
   );
--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -2,7 +2,7 @@ import SignUpForm from "../_components/signUpForm/page";
 
 const SignUp = () => {
   return (
-    <div className="flex justify-center bg-[#F5F5F5] px-4">
+    <div className="flex justify-center bg-[#F5F5F5] px-4 py-4">
       <SignUpForm />
     </div>
   );
