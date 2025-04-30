4c02fed25a6a8ec4b5135b49d2a0a11f6b736278
chindesu0207 <147689292+chindesu0207@users.noreply.github.com>
2025-04-30 16:16:30 +0800

    refactor: 💡 調整為類似設計稿的樣式

--- src/components/layout/Footer/_components/Sitemap/MobileSitemap.tsx
+++ src/components/layout/Footer/_components/Sitemap/MobileSitemap.tsx
@@ -10,20 +10,16 @@ const MobileSitemap = ({ data }: SitemapProps) => {
   const [open, setOpen] = useState("");
 
   return (
-    <Accordion.Root type="single" value={open} onValueChange={setOpen} className="md:hidden" collapsible>
+    <Accordion.Root type="single" value={open} onValueChange={setOpen} className="w-full md:hidden" collapsible>
       {data.map((group) => (
         <Accordion.Item key={group.title} value={group.title}>
-          <Accordion.Trigger className="text-secondary-300 flex w-full items-center justify-between border-b px-6 py-3">
+          <Accordion.Trigger className="text-secondary-300 flex w-full items-center justify-between py-3">
             <div className="text-left text-sm font-bold">{group.title}</div>
             {open === group.title ? <ChevronUp className="size-5" /> : <ChevronDown className="size-5" />}
           </Accordion.Trigger>
-          <Accordion.Content className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down bg-secondary-300 flex flex-col overflow-hidden">
+          <Accordion.Content className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down flex flex-col overflow-hidden">
             {group.items.map((item) => (
-              <Link
-                className="border-secondary border-b px-6 py-3 text-left text-sm text-black"
-                key={item.label}
-                href={item.href}
-              >
+              <Link className="py-3 text-left text-sm font-bold text-white" key={item.label} href={item.href}>
                 {item.label}
               </Link>
             ))}
