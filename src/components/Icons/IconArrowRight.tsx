6817e9236f388a01f1dd3ceb72565666e1d8a45c
chindesu0207 <147689292+chindesu0207@users.noreply.github.com>
2025-04-30 16:24:09 +0800

    feat: 🎸 新增左右箭頭的 icon 元件

---
+++ src/components/Icons/IconArrowRight.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IconArrowRight = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+      <path d="M1 14C1 13.7239 1.22386 13.5 1.5 13.5H22.5C22.7761 13.5 23 13.7239 23 14C23 14.2761 22.7761 14.5 22.5 14.5H1.5C1.22386 14.5 1 14.2761 1 14Z" />
+      <path d="M13.1464 9.14645C13.3417 8.95118 13.6583 8.95118 13.8536 9.14645L18.0962 13.3891C18.2915 13.5843 18.2915 13.9009 18.0962 14.0962C17.9009 14.2915 17.5844 14.2915 17.3891 14.0962L13.1464 9.85355C12.9512 9.65829 12.9512 9.34171 13.1464 9.14645Z" />
+    </svg>
+  );
+};
+
+export default IconArrowRight;
