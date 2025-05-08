aae29c872000ec9e3e24871ee3019560a948c38b
Lobinda <lobinda123@gmail.com>
2025-05-09 01:25:59 +0800

    fix:file name

--- src/app/(auth)/login/_components/LoginForm/page.tsx
+++ src/app/(auth)/login/_components/LoginForm/page.tsx
@@ -9,7 +9,7 @@ import { useLoginMutation } from "@/hooks/react-query/useLoginMutation";
 import { LoginRequestSchema, LoginRequestSchemaType } from "@/schema/auth.dto";
 import { useLoginStore } from "@/store/useLoginStore";
 
-import { LoginFieldType, loginFields } from "../../login";
+import { LoginFieldType, loginFields } from "../../loginFields";
 import GoogleButton from "../GoogleButton";
 import LineButton from "../LineButton";
 import FbButton from "../fbButton";
