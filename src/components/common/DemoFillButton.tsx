c6108e8c1df3c72d59e0f0e20159c2b47042c5f6
chindesu0207 <147689292+chindesu0207@users.noreply.github.com>
2025-05-09 12:40:50 +0800

    feat: 🎸 新增 demo 用來填入資料的 button

---
+++ src/components/common/DemoFillButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+export type DemoFieldItem = Record<string, string | number | boolean>;
+type DemoFillButtonProps = {
+  fields: DemoFieldItem[];
+};
+
+const DemoFillButton = ({ fields }: DemoFillButtonProps) => {
+  const { setValue } = useFormContext();
+  const handleFill = () => {
+    fields.forEach((field) => {
+      const [key, value] = Object.entries(field)[0];
+      setValue(key, value);
+    });
+
+    // setValue("email", "lobinda@gmail.com");
+    // setValue("password", "11111111");
+    // setValue("rememberMe", true);
+  };
+  return (
+    <div
+      className="fixed top-14 right-4 z-40 h-10 w-10 cursor-pointer rounded-full bg-red-500 shadow-md transition-all duration-300 hover:bg-red-600 md:h-10 md:w-10"
+      onClick={handleFill}
+    ></div>
+  );
+};
+
+export default DemoFillButton;
