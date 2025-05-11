c54eb04f8bafb7e27e637e2f42503841991bd383
Lobinda <lobinda123@gmail.com>
2025-05-11 16:16:09 +0800

    fix: 🐛 修改存取位置

--- src/components/FormRender/index.tsx
+++ src/components/FormRender/index.tsx
@@ -68,16 +68,17 @@ const FormRender = <T extends FieldValues>({ FormFields }: FormRendererProps<T>)
                       <div className="relative w-full">
                         <Input
                           id={field.id}
-                          className="focus:border-primary"
+                          className="focus:border-primary pr-10"
                           type={isPassword ? (showPasswordMap[fieldKey] ? "text" : "password") : field.type}
                           placeholder={field.placeholder}
                           {...rhfField}
                         />
+
                         {isPassword && (
                           <button
                             type="button"
                             onClick={() => togglePassword(fieldKey)}
-                            className="hover:text-primary absolute top-1/2 right-4 -translate-y-1/2 text-gray-500"
+                            className="hover:text-primary text-gray-cap absolute top-1/2 right-3 z-10 -translate-y-1/2"
                           >
                             {showPasswordMap[fieldKey] ? <EyeOff size={20} /> : <Eye size={20} />}
                           </button>
