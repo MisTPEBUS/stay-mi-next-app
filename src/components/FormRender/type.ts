6074e56f372affb771230e0903f9bdd8adaead8d
Lobinda <lobinda123@gmail.com>
2025-05-25 17:27:15 +0800

    fix:修改formRender欄位名稱+type

--- src/components/FormRender/type.ts
+++ src/components/FormRender/type.ts
@@ -1,27 +1,41 @@
 import { FieldValues, Path } from "react-hook-form";
-export type FieldType = "text" | "tel" | "password" | "radio" | "select" | "checkbox" | "textarea" | "email" | "date";
+
+export type FieldType =
+  | "text"
+  | "tel"
+  | "password"
+  | "number"
+  | "email"
+  | "date"
+  | "radio"
+  | "select"
+  | "checkbox"
+  | "switch"
+  | "editor"
+  | "file"
+  | "textarea";
 
 export type FormFieldOption = {
   label: string;
   value: string;
 };
 
-export type FormFieldConfig<T extends FieldValues = FieldValues> = {
+export type FormFieldConfig<T extends FieldValues> = {
   name: Path<T>;
   type: FieldType;
   label?: string;
   placeholder?: string;
-  options?: FormFieldOption[];
-  text?: string;
+  options?: FormFieldOption[]; // radio/select/checkbox
+  description?: string;
   id?: string;
-  key?: string;
-  halfWidth?: boolean;
-  disabled?: boolean;
-  required?: boolean;
   className?: string;
   labelClassName?: string;
+  disabled?: boolean;
+  required?: boolean;
+  halfWidth?: boolean;
+  colSpan?: number;
 };
 
-export type FormRendererProps<T extends FieldValues> = {
-  FormFields: FormFieldConfig<T>[];
+export type FormRenderProps<T extends FieldValues> = {
+  fields: FormFieldConfig<T>[];
 };
