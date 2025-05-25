914f1e22e7ef39fe943eeb62e94e10c0e21ecc27
Lobinda <lobinda123@gmail.com>
2025-05-25 17:38:35 +0800

    fix:元件修改成field.type, switch-case管理

--- src/components/FormRender/index.tsx
+++ src/components/FormRender/index.tsx
@@ -1,100 +1,53 @@
-"use client";
+import { FieldValues } from "react-hook-form";
 
-import { Eye, EyeOff } from "lucide-react";
-import { useState } from "react";
-import { FieldValues, useFormContext } from "react-hook-form";
+import FormCheckbox from "./FormCheckbox";
+import FormDatePicker from "./FormDatePicker";
+import FormInput from "./FormInput";
+import FormRadioGroup from "./FormRadioGroup";
+import { FormSelect } from "./FormSelect";
+import FormSwitch from "./FormSwitch";
+import FormTextarea from "./FormTextarea";
+import { FormTipTapEditor } from "./FormTipTapEditor";
+import { FormRenderProps } from "./type";
 
-import { Checkbox } from "@/components/ui/checkbox";
-import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-
-import { FormRendererProps } from "./type";
-
-const FormRender = <T extends FieldValues>({ FormFields }: FormRendererProps<T>) => {
-  const [showPasswordMap, setShowPasswordMap] = useState<Record<string, boolean>>({});
-  const { control } = useFormContext<T>();
-
-  const togglePassword = (name: string) => {
-    setShowPasswordMap((prev) => ({
-      ...prev,
-      [name]: !prev[name],
-    }));
-  };
-
+export const FormRender = <T extends FieldValues>({ fields }: FormRenderProps<T>) => {
   return (
-    <div className="flex flex-wrap gap-4">
-      {FormFields.map((field) => {
-        const isPassword = field.type === "password";
-        const fieldKey = String(field.name);
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      {fields.map((field) => {
+        const colSpan = field.halfWidth ? "col-span-1" : "col-span-2";
+        const renderField = () => {
+          switch (field.type) {
+            case "text":
+            case "tel":
+            case "email":
+            case "password":
+            case "number":
+              return <FormInput key={field.name} field={field} />;
+            case "radio":
+              return <FormRadioGroup key={field.name} field={field} />;
+            case "select":
+              return <FormSelect key={field.name} field={field} />;
+            case "checkbox":
+              return <FormCheckbox key={field.name} field={field} />;
+            case "textarea":
+              return <FormTextarea key={field.name} field={field} />;
+            case "switch":
+              return <FormSwitch key={field.name} field={field} />;
+            case "date":
+              return <FormDatePicker key={field.name} field={field} />;
+            case "editor":
+              return <FormTipTapEditor key={field.name} name={field.name} label={field.label || ""} />;
+            default:
+              return null;
+          }
+        };
 
         return (
-          <div key={field.key || fieldKey} className={cn(field.halfWidth ? "w-full md:w-[48%]" : "w-full")}>
-            <FormField
-              name={field.name}
-              control={control}
-              render={({ field: rhfField }) => (
-                <FormItem className={cn("", field.className)}>
-                  {field.type !== "checkbox" && field.label && (
-                    <FormLabel className={cn("text-black-sub block text-base font-medium", field.labelClassName)}>
-                      {field.label}
-                    </FormLabel>
-                  )}
-
-                  <FormControl>
-                    {/*   checkbox */}
-                    {field.type === "checkbox" ? (
-                      <div className="flex items-center space-x-2">
-                        <Checkbox id={field.id} checked={!!rhfField.value} onCheckedChange={rhfField.onChange} />
-                        <Label htmlFor={field.id}>{field.label}</Label>
-                      </div>
-                    ) : field.type === "radio" && field.options ? (
-                      <div className="space-y-1">
-                        {field.options.map((opt) => (
-                          <label key={opt.value} className="flex items-center space-x-2">
-                            <input
-                              type="radio"
-                              value={opt.value}
-                              checked={rhfField.value === opt.value}
-                              onChange={() => rhfField.onChange(opt.value)}
-                              name={String(field.name)}
-                            />
-                            <span>{opt.label}</span>
-                          </label>
-                        ))}
-                      </div>
-                    ) : (
-                      <div className="relative w-full">
-                        <Input
-                          id={field.id}
-                          className="focus:border-primary pr-10"
-                          type={isPassword ? (showPasswordMap[fieldKey] ? "text" : "password") : field.type}
-                          placeholder={field.placeholder}
-                          {...rhfField}
-                        />
-
-                        {isPassword && (
-                          <button
-                            type="button"
-                            onClick={() => togglePassword(fieldKey)}
-                            className="hover:text-primary text-gray-cap bg-white-pure absolute top-1/2 right-3 -translate-y-1/2"
-                          >
-                            {showPasswordMap[fieldKey] ? <EyeOff size={20} /> : <Eye size={20} />}
-                          </button>
-                        )}
-                      </div>
-                    )}
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <div key={field.name} className={colSpan}>
+            {renderField()}
           </div>
         );
       })}
     </div>
   );
 };
-
-export default FormRender;
