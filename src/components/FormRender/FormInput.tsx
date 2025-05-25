c25d2806001deb230c070715cb0e680a7b017427
Lobinda <lobinda123@gmail.com>
2025-05-25 17:58:23 +0800

    feat:新增密碼眼睛處理

--- src/components/FormRender/FormInput.tsx
+++ src/components/FormRender/FormInput.tsx
@@ -1,3 +1,5 @@
+import { Eye, EyeOff } from "lucide-react"; // shadcn 預設使用的 icon 套件
+import { useState } from "react";
 import { FieldValues, useFormContext } from "react-hook-form";
 
 import { FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
@@ -12,6 +14,10 @@ type FormInputProps<T extends FieldValues> = {
 
 const FormInput = <T extends FieldValues>({ field }: FormInputProps<T>) => {
   const form = useFormContext<T>();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const isPassword = field.type === "password";
+  const inputType = isPassword ? (showPassword ? "text" : "password") : field.type || "text";
 
   return (
     <FormField
@@ -25,11 +31,24 @@ const FormInput = <T extends FieldValues>({ field }: FormInputProps<T>) => {
             </FormLabel>
           )}
           <FormControl>
-            <Input
-              placeholder={field.placeholder}
-              {...controller}
-              className="focus:border-primary active:ring-primary pr-10 active:ring"
-            />
+            <div className="relative">
+              <Input
+                {...controller}
+                placeholder={field.placeholder}
+                type={inputType}
+                className={cn("focus:border-primary active:ring-primary pr-10 active:ring", field.className)}
+              />
+              {isPassword && (
+                <button
+                  type="button"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  className="absolute top-1/2 right-2 -translate-y-1/2 text-gray-500"
+                  tabIndex={-1}
+                >
+                  {showPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
+                </button>
+              )}
+            </div>
           </FormControl>
           {field.description && <FormDescription>{field.description}</FormDescription>}
           <FormMessage />
@@ -38,4 +57,5 @@ const FormInput = <T extends FieldValues>({ field }: FormInputProps<T>) => {
     />
   );
 };
+
 export default FormInput;
