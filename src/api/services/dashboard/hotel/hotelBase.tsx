7d2954bbcc010409b60614849648e996653e1657
Lobinda <lobinda123@gmail.com>
2025-06-01 23:50:00 +0800

    feat: 飯店新增修改刪除API

---
+++ src/api/services/dashboard/hotel/hotelBase.tsx
@@ -0,0 +1,34 @@
+import { AxiosResponse } from "axios";
+
+import AxiosStoreClient from "@/api/axios/axiosStoreClient";
+import AxiosUserClient from "@/api/axios/axiosUserClient";
+import { CreateHotelSchemaType, HotelListSchemaType } from "@/schema/dashboard/hotelBase.dto";
+import {
+  UpdatePasswordSchemaType,
+  UpdateUserProfileReqSchemaType,
+  UserProfileResponseSchemaType,
+} from "@/schema/userProfile.dto";
+
+type updateHotelBaseProps = {
+  hotelInfo: CreateHotelSchemaType;
+  id: string;
+};
+
+export const HotelBaseApi = {
+  getHotelBase: async (): Promise<AxiosResponse<HotelListSchemaType>> => {
+    const response = await AxiosStoreClient.get<HotelListSchemaType>("/hotel");
+
+    console.log(response);
+    return response;
+  },
+  createHotelBase: async (hotelInfo: CreateHotelSchemaType): Promise<AxiosResponse> => {
+    const response = await AxiosStoreClient.post("/hotel", hotelInfo);
+    console.log(response);
+    return response;
+  },
+  updateHotelBase: async ({ hotelInfo, id }: updateHotelBaseProps): Promise<AxiosResponse> => {
+    const response = await AxiosStoreClient.put(`/hotel/${id}`, hotelInfo);
+    console.log(response);
+    return response;
+  },
+};
