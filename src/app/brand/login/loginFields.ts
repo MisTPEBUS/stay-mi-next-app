82ecc28f35b62a8a9790dd14abe17c37b5aa026b
Lobinda <lobinda123@gmail.com>
2025-06-01 23:52:25 +0800

    feat:商家登入欄位

--- src/app/brand/login/loginFields.ts
+++ src/app/brand/login/loginFields.ts
@@ -2,8 +2,27 @@ import { FormFieldConfig } from "@/components/FormRender/type";
 import { LoginRequestSchemaType } from "@/schema/auth.dto";
 
 export type LoginFieldType = LoginRequestSchemaType & { rememberMe: boolean };
-
+export type LoginStoreFieldType = LoginRequestSchemaType;
 export const loginFields: FormFieldConfig<LoginFieldType>[] = [
+  {
+    type: "email",
+    label: "帳號",
+    name: "email",
+    placeholder: "請輸入信箱",
+    required: true,
+    className: "mb-2",
+  },
+  {
+    type: "password",
+    label: "密碼",
+    name: "password",
+    placeholder: "請輸入密碼",
+    required: true,
+    className: "mb-2",
+  },
+];
+
+export const loginStoreFields: FormFieldConfig<LoginStoreFieldType>[] = [
   {
     type: "email",
     label: "帳號",
