66b189ace5ead37631db274ca30fbc25b1114282
Lobinda <lobinda123@gmail.com>
2025-06-14 11:01:35 +0800

    feat:新增demo btn

--- src/app/dashboard/hotel-room/_components/HotelRoomEditorDialog.tsx
+++ src/app/dashboard/hotel-room/_components/HotelRoomEditorDialog.tsx
@@ -5,6 +5,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 import { FormRender } from "@/components/FormRender";
+import DemoFillButton, { DemoFieldItem } from "@/components/common/DemoFillButton";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useCreateHotelRoom, useUpdateHotelRoom } from "@/hooks/react-query/useRoom";
@@ -13,6 +14,18 @@ import { HotelRoomTypeCreateType } from "@/schema/dashboard/hotelRoom.dto";
 import { useRoomDialogStore } from "@/store/Dialog/useRoomStore";
 
 import { dialogFields as staticDialogFields } from "./dialogFields";
+const HotelRoomDemoData: DemoFieldItem[] = [
+  { basePrice: 3000 },
+  { is_active: true },
+  {
+    description: `<h2>房型特色</h2><ol class="list-decimal ml-3"><li><p>專屬芬多精香氛私人陽台</p></li><li><p>6尺特製雙浴缸，舒壓雙人大床</p></li><li><p>獨立強勁出風量空調立扇</p></li><li><p>豪華藍牙喇叭與氣氛燈泡</p></li><li><p>55 吋智慧電視 | Netflix 支援</p></li></ol><hr><h2>貼心服務</h2><ol class="list-decimal ml-3"><li><p>免費專屬停車位</p></li><li><p>房內設施配有 Dyson整髮神器</p></li><li><p>可提供加價專屬 SPA、泡湯設置</p></li></ol>`,
+  },
+  { id: "" },
+  { hotel_id: "" },
+  { room_type_id: "" },
+
+  { images: [] },
+];
 
 export const HotelRoomEditorDialog = () => {
   const methods = useForm<HotelRoomTypeCreateType>();
@@ -77,6 +90,7 @@ export const HotelRoomEditorDialog = () => {
 
         <div className="flex-1 overflow-y-auto px-6 py-4">
           <FormProvider {...methods}>
+            <DemoFillButton fields={HotelRoomDemoData} />
             <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
               <FormRender<HotelRoomTypeCreateType> fields={dialogFields} />
             </form>
@@ -84,7 +98,14 @@ export const HotelRoomEditorDialog = () => {
         </div>
 
         <div className="space-x-2 border-t p-6 text-end">
-          <Button type="submit" form="my-form-id" disabled={isSubmitting} size="lg" className="rounded-md">
+          <Button
+            type="submit"
+            form="my-form-id"
+            disabled={isSubmitting}
+            size="lg"
+            className="rounded-md"
+            onClick={handleSubmit(onSubmit)}
+          >
             {isEdit ? "更新" : "儲存"}
           </Button>
           <Button
