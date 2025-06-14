7e64846506561e8caed810bf71679413d75b2556
Lobinda <lobinda123@gmail.com>
2025-06-14 23:12:50 +0800

    fix:修正連結

--- src/hooks/react-query/useLoginMutation.ts
+++ src/hooks/react-query/useLoginMutation.ts
@@ -8,12 +8,12 @@ import { LoginResponseData } from "@/api/services/auth/type";
 import { ErrorResponse } from "@/api/type";
 import { LoginRequestSchemaType, StoreLoginRequestSchemaType } from "@/schema/auth.dto";
 import { useAuthStore } from "@/store/useAuthStore";
-import { useLoginStore } from "@/store/useLoginStore";
+import { useLoginStore, useStoreLoginStore } from "@/store/useLoginStore";
 
 export const useLoginStoreMutation = () => {
   const setUser = useAuthStore.getState().setUser;
   const router = useRouter();
-  const loginStore = useLoginStore.getState();
+  const loginStore = useStoreLoginStore.getState();
 
   return useMutation({
     mutationFn: (data: StoreLoginRequestSchemaType): Promise<AxiosResponse<LoginResponseData>> =>
@@ -29,7 +29,7 @@ export const useLoginStoreMutation = () => {
       loginStore.setEmail(variables.email);
 
       toast.success("登入成功");
-      router.push("/dashboard/hotel");
+      router.push("/dashboard");
     },
 
     onError: (err) => {
