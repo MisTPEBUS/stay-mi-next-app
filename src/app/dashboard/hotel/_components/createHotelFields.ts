2166ab31607eab64b113d8457d8b5d03d26243ef
Lobinda <lobinda123@gmail.com>
2025-06-14 10:54:15 +0800

    pref:擴增option

--- src/app/dashboard/hotel/_components/createHotelFields.ts
+++ src/app/dashboard/hotel/_components/createHotelFields.ts
@@ -1,5 +1,5 @@
 import { FormFieldConfig } from "@/components/FormRender/type";
-import { hotelFacilities } from "@/config/settings";
+import { cityOptions, hotelFacilities } from "@/config/settings";
 import { CreateHotelFormSchemaType } from "@/schema/dashboard/hotelBase.dto";
 
 const hotelFacilityOptions = hotelFacilities.map((item) => ({
@@ -11,15 +11,8 @@ export const createHotelFields: FormFieldConfig<CreateHotelFormSchemaType>[] = [
     name: "region",
     type: "select",
     label: "地區",
-    placeholder: "請輸入地區，例如 台北",
-    options: [
-      { label: "台北市", value: "台北市" },
-      { label: "新北市", value: "新北市" },
-      { label: "台中市", value: "台中市" },
-      { label: "新竹市", value: "新竹市" },
-      { label: "新竹縣", value: "新竹縣" },
-      { label: "高雄市", value: "高雄市" },
-    ],
+    placeholder: "請輸入地區，例如 台北, 新北, 高雄市",
+    options: cityOptions,
     halfWidth: true,
   },
   {
