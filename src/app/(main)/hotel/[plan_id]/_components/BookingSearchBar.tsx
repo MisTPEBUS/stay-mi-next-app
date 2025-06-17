ef8a704d8eedb49d2f64a315b58ba429f50fee53
Lobinda <lobinda123@gmail.com>
2025-06-18 02:17:24 +0800

    feat: 🎸 建立BOOKING 查詢房間SEARCH BAR

---
+++ src/app/(main)/hotel/[plan_id]/_components/BookingSearchBar.tsx
@@ -0,0 +1,34 @@
+import { BedDouble, Hotel, MapPin } from "lucide-react";
+import React from "react";
+
+import DatePickerWithRange from "@/app/(main)/(home)/sections/Hero/_components/HeroSearchBar/_component/DatePickerWithRange";
+import { Button } from "@/components/ui/button";
+
+const BookingSearchBar = () => {
+  return (
+    <div className="bg-white-pure flex flex-col items-center rounded-xl p-2 font-bold md:flex-row">
+      <div className="flex w-full items-center gap-2 p-5 md:w-1/5">
+        <Hotel className="size-6" />
+        <input
+          type="text"
+          placeholder="請輸入飯店名稱"
+          className="w-full text-base text-black placeholder-black outline-none"
+        />
+      </div>
+      <div className="flex w-full items-center gap-2 p-5 md:w-1/5">
+        <MapPin className="size-6" />
+        <input type="text" placeholder="請選擇地點" className="w-full text-base placeholder-black outline-none" />
+      </div>
+      <DatePickerWithRange className="w-full md:w-3/10" />
+      <div className="flex w-full items-center gap-2 p-5 md:w-1/5">
+        <BedDouble className="size-6" />
+        <input type="text" placeholder="請選擇房型" className="w-full text-base placeholder-black outline-none" />
+      </div>
+      <div className="flex w-full items-center gap-2 md:w-1/10">
+        <Button className="h-16 w-full rounded-lg">立即搜尋</Button>
+      </div>
+    </div>
+  );
+};
+
+export default BookingSearchBar;
