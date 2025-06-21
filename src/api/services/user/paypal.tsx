9052109558e587fe8e6d2a69e8d4ae930c23aa62
Lobinda <lobinda123@gmail.com>
2025-06-22 03:04:04 +0800

    fix:修改type

--- src/api/services/user/paypal.tsx
+++ src/api/services/user/paypal.tsx
@@ -2,25 +2,23 @@ import { AxiosResponse } from "axios";
 
 import AxiosUserClient from "@/api/axios/axiosUserClient";
 import { SubmitOrderSchemaType } from "@/app/(main)/check-order/types";
+import { OrderDetailType } from "@/schema/dashboard/order.dto";
 
 export type createPaypalOrderRes = {
   orderId: string;
   approveLink: string;
 };
-export type createCaptureOrderByID = {
-  order_type: string;
-  method: string;
-};
+
 export const UserPaypalApi = {
   createPaypalOrder: async (params: SubmitOrderSchemaType): Promise<createPaypalOrderRes> => {
     const response = await AxiosUserClient.post<createPaypalOrderRes>(`/paypal/create-order`, params);
 
     return response.data;
   },
-  createCaptureOrderByID: async (id: string): Promise<createCaptureOrderByID> => {
+  createCaptureOrderByID: async (id: string): Promise<OrderDetailType> => {
     //paymentID
-    const response = await AxiosUserClient.post<AxiosResponse<createCaptureOrderByID>>(`/paypal/capture-order/${id}`);
+    const response = await AxiosUserClient.post<{ payment: OrderDetailType }>(`/paypal/capture-order/${id}`);
     console.log(response);
-    return response.data.data;
+    return response.data.payment;
   },
 };
