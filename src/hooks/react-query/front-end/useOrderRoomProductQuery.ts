0501b6e2a567db2d1735f19a832fa7205fdf2483
Lobinda <lobinda123@gmail.com>
2025-06-25 16:39:33 +0800

    feat:建立react-query

--- src/hooks/react-query/front-end/useOrderRoomProductQuery.ts
+++ src/hooks/react-query/front-end/useOrderRoomProductQuery.ts
@@ -3,6 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { OrderRoomProductApi } from "@/api/services/user/plan/OrderRoomProduct";
 import { OrderDetailType } from "@/schema/dashboard/order.dto";
 
+type Props = {
+  status: "all" | "pending" | "confirmed" | "cancelled";
+};
 export const useOrderRoomProductQuery = (id: string | undefined) => {
   return useQuery<OrderDetailType>({
     queryKey: ["order-room-product", id],
@@ -11,6 +14,19 @@ export const useOrderRoomProductQuery = (id: string | undefined) => {
       return res;
     },
     enabled: !!id,
+    staleTime: 1000 * 60 * 3,
+    retry: 1,
+  });
+};
+
+export const useOrderRoomProductQueryAll = ({ status }: Props) => {
+  return useQuery<OrderDetailType>({
+    queryKey: ["order-room-product", status],
+    queryFn: async () => {
+      const res = await OrderRoomProductApi.getAllOrderRoomProduct({ status });
+      return res;
+    },
+    enabled: !!status,
     staleTime: 1000 * 60 * 3,
     retry: 1,
   });
